9f1792bccab006c68de2dbe1604e102589ed07a5
ryan-santiago <ryan.santiago.ict@gmail.com>
2026-06-28 16:16:04 +0800

    09:agent-setup

--- src/modules/home/ui/views/home-view.tsx
+++ src/modules/home/ui/views/home-view.tsx
@@ -1,12 +1,7 @@
 'use client'
 
-import { useTRPC } from '@/trpc/client'
-import { useQuery } from '@tanstack/react-query'
 import React from 'react'
 
 export const HomeView = () => {
-	const trpc = useTRPC()
-	const { data } = useQuery(trpc.hello.queryOptions({ text: 'Ryan' }))
-
-	return <div className="p-4 flex flex-col gap-y-4">{data?.greeting}</div>
+	return <div>Home view Test</div>
 }
